78a13104878c70c917b43f713b72c33ef07cb397
Johannes Emerich <johannes@emerich.de>
2012-01-08 22:19:51 +0100

    Add unit test for response templating

--- test/test_response.rb
+++ test/test_response.rb
@@ -4,19 +4,25 @@ require 'second_mate'
 
 class TestResponse < Test::Unit::TestCase
 
-  def setup
-    @response = SecondMate::Response.new(
+  def test_yaml_frontmatter
+    response = SecondMate::Response.new(
       "---\nstatus: 201\nheader:\n  Foo: Baz\n---\nBody",
       {},
       200,
       {'Foo' => 'Bar'}
-    )
-  end
-
-  def test_yaml_frontmatter
-    response = @response.finish
+    ).finish
     assert_equal ['Body'], response.last.body
     assert_equal 'Baz', response[1]['Foo']
+  end
+
+  def test_templating
+    response = SecondMate::Response.new(
+      "<%= params['foo'] %>",
+      {'foo' => 'Bar'},
+      200,
+      {}
+    ).finish
+    assert_equal ['Bar'], response.last.body
   end
 
 end
